6f894ae591667eaa04213a589ec8ed8f60f5ea82
a.cheremushkin <a.ch3re359@yandex.ru>
2023-02-23 13:16:23 +0300

    Remove pycrypto from requirements.txt

--- database.py
+++ database.py
@@ -3,11 +3,15 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
 load_dotenv()
 
+if os.getenv('CI') is None:
+    DATABASE_URL = os.getenv('DATABASE_URL')
+else:
+    DATABASE_URL = 'sqlite:///' + str(settings.BASE_DIR) + '/db.sqlite'
 
-DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(engine)
